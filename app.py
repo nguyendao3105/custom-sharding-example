9ad320b4a29cf5051707441ea8a71ce663bf731e
Dao Nguyen <dao.nguyen@paradox.ai>
2022-09-28 16:09:50 +0700

    can now insert to databases

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from typing import Union
 from webbrowser import get
-
+import hashlib
 from fastapi import FastAPI
 
 from db.db import get_connection
@@ -9,9 +9,19 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root(q: Union[str, None] = None):
-    s = get_connection(q)
-    return {"Hello": str(s)}
+def get():
+    return {"msg": "hello stranger"}
+
+@app.post("/")
+def post_url(q: Union[str, None] = None):
+    if q:
+        hashed_string = hashlib.sha256(q.encode('utf-8')).hexdigest()[0:5]
+        conn, node = get_connection(q)
+        cur = conn.cursor()
+        cur.execute(f"INSERT INTO url_store(url, short_url) VALUES('{q}', '{hashed_string}')")
+        conn.commit()
+        return {"value": "q", "node": node['name']}
+    return {"msg": "insert something mother fucker"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
--- db/db.py
+++ db/db.py
@@ -4,28 +4,42 @@ from uhashring import HashRing
 simple_node_list = [
     {
         "name": "node_1",
-        "host": "localhost",
+        "host": "127.0.0.1",
         "dbname": "postgres_1",
         "user": "postgres_1",
-        "password": "postgres_1"
+        "password": "postgres_1",
+        "port": "5432"
     },
     {
         "name": "node_2",
-        "host": "localhost",
+        "host": "127.0.0.1",
         "dbname": "postgres_2",
         "user": "postgres_2",
-        "password": "postgres_2"
+        "password": "postgres_2",
+        "port": "5433"
     },
     {
         "name": "node_3",
-        "host": "localhost",
+        "host": "127.0.0.1",
         "dbname": "postgres_3",
         "user": "postgres_3",
-        "password": "postgres_3"
+        "password": "postgres_3",
+        "port": "5434"
     }
 ]
 
 def get_connection(value):
     nodes = list(map(lambda node: node["name"], simple_node_list))
     ring = HashRing(nodes=nodes)
-    return ring.get_node(value)
+    node_name = ring.get_node(value)
+    node = get_node_properties(node_name=node_name, node_list=simple_node_list)
+    connection_string = f"host={node['host']} port={node['port']} dbname={node['dbname']} user={node['user']} password={node['password']}"
+    print(connection_string)
+    return psycopg2.connect(connection_string), node
+
+def get_node_properties(node_name, node_list):
+    for node in node_list:
+        if node["name"] == node_name:
+            return node
+    return None
+    
